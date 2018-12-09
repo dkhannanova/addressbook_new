db58748408f801858b968f4c4e08e750c9b5e443
kdm <dkhannanovam@gmail.com>
2018-12-09 16:23:21 +0300

    отдельный класс Контаккт

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname,
+                 nickname, photo, title, company, address,
+                 home, mobile, work, fax, email, homepage, bday, bmonth, byear, address2, phone, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.address2 = address2
+        self.phone = phone
+        self.notes = notes
+
+
+
+
+
+
