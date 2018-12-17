74e31f09fb2dd34bee722867bdf3d9f3befb98ee
kdm <dkhannanovam@gmail.com>
2018-12-17 21:37:51 +0300

    выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,11 +13,11 @@ def app(request):
 
     
 def test_add_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_new_contact(Contact(firstname="1", middlename="middlename", lastname="lastname", nickname="nickname", photo="C:\\Users\hannanovadm\Pictures\9908383.pdf", title="title",
                                             company="company", address="address", home="home", mobile="mobile", work="work", fax="fax", email="email",
                                             homepage="homepage", bday="1", bmonth="August", byear="1985", address2="address2", phone="phone2", notes="notes"))
 
-        app.logout()
+        app.session.logout()
 
 
