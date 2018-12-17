744e85dd50a79b418a0dff1922f8e41d5014a211
kdm <dkhannanovam@gmail.com>
2018-12-17 23:10:59 +0300

    вынесена фикстура

--- test/test_add_group_0812.py
+++ test/test_add_group_0812.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
-import pytest
-from model.group import Group
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+from model.group import Group
 
 def test_add_group0812(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(group_name="name1", group_header="header1", group_footer="footer1"))
+    app.group.create(Group(group_name="name1", group_header="header1", group_footer="footer1"))
     app.session.logout()
 
 def test_add_group0812_empty(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(group_name="", group_header="", group_footer=""))
+    app.group.create(Group(group_name="", group_header="", group_footer=""))
     app.session.logout()
 
