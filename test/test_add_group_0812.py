8e3ebc4caf4dd9902bed129b23091948a79777dc
kdm <dkhannanovam@gmail.com>
2018-12-17 21:38:20 +0300

    выделен класс для управления сессиями

--- test/test_add_group_0812.py
+++ test/test_add_group_0812.py
@@ -10,12 +10,12 @@ def app(request):
     return fixture
 
 def test_add_group0812(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(group_name="name1", group_header="header1", group_footer="footer1"))
-    app.logout()
+    app.session.logout()
 
 def test_add_group0812_empty(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(group_name="", group_header="", group_footer=""))
-    app.logout()
+    app.session.logout()
 
