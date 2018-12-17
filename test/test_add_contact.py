b62893eca0fc4038c1c48d8faa6b6bec3ad003c6
kdm <dkhannanovam@gmail.com>
2018-12-17 22:23:22 +0300

    выделены классы помощники для создания группы и создания контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,7 +14,7 @@ def app(request):
     
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
-        app.create_new_contact(Contact(firstname="1", middlename="middlename", lastname="lastname", nickname="nickname", photo="C:\\Users\hannanovadm\Pictures\9908383.pdf", title="title",
+        app.contact.create_new_contact(Contact(firstname="1", middlename="middlename", lastname="lastname", nickname="nickname", photo="C:\\Users\hannanovadm\Pictures\9908383.pdf", title="title",
                                             company="company", address="address", home="home", mobile="mobile", work="work", fax="fax", email="email",
                                             homepage="homepage", bday="1", bmonth="August", byear="1985", address2="address2", phone="phone2", notes="notes"))
 
