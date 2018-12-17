3996b66a6174244023c878635780da44d74a71fb
kdm <dkhannanovam@gmail.com>
2018-12-17 23:10:00 +0300

    вынесена фикстура

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
-    
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
         app.contact.create_new_contact(Contact(firstname="1", middlename="middlename", lastname="lastname", nickname="nickname", photo="C:\\Users\hannanovadm\Pictures\9908383.pdf", title="title",
