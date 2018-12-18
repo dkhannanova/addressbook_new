6b895cf69dd7a9817f3993b9f7d0720b0525679d
kdm <dkhannanovam@gmail.com>
2018-12-18 19:28:42 +0300

    Создан класс помощник для модификации

---
+++ fixture/group.py
@@ -0,0 +1,63 @@
+class GroupHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_groups_page(self):
+        # return to group page
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group fields
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
+        wd.find_element_by_xpath("//div[@id='content']/form").click()
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.group_header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
+        # submit group data
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #delete group
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def modify_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # modify group
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
+        wd.find_element_by_xpath("//div[@id='content']/form").click()
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.group_header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
+        # submit group data
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        # open groups page
+        wd.find_element_by_link_text("groups").click()
