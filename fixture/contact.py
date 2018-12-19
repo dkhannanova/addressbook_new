ff3fb9e163fa9004cd54286f87e8a446348fd706
kdm <dkhannanovam@gmail.com>
2018-12-19 11:40:22 +0300

    заполнение формы контакта в отдельном методе

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,8 +5,7 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def create_new_contact(self, contact):
-        # create contact
+    def fill_contact(self, contact):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
@@ -65,6 +64,11 @@ class ContactHelper:
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
+
+    def create_new_contact(self, contact):
+        # create contact
+        wd = self.app.wd
+        self.fill_contact(contact)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def delete_contact(self):
@@ -76,61 +80,6 @@ class ContactHelper:
 
     def modify_contact(self, contact):
             wd = self.app.wd
+            self.fill_contact(contact)
             wd.find_element_by_xpath("//img[@alt='Edit']").click()
-            wd.find_element_by_name("firstname").click()
-            wd.find_element_by_name("firstname").clear()
-            wd.find_element_by_name("firstname").send_keys(contact.firstname)
-            wd.find_element_by_name("middlename").click()
-            wd.find_element_by_name("middlename").clear()
-            wd.find_element_by_name("middlename").send_keys(contact.middlename)
-            wd.find_element_by_name("lastname").click()
-            wd.find_element_by_name("lastname").clear()
-            wd.find_element_by_name("lastname").send_keys(contact.lastname)
-            wd.find_element_by_name("nickname").click()
-            wd.find_element_by_name("nickname").clear()
-            wd.find_element_by_name("nickname").send_keys(contact.nickname)
-            wd.find_element_by_name("photo").send_keys(contact.photo)
-            wd.find_element_by_name("title").click()
-            wd.find_element_by_name("title").clear()
-            wd.find_element_by_name("title").send_keys(contact.title)
-            wd.find_element_by_name("company").click()
-            wd.find_element_by_name("company").clear()
-            wd.find_element_by_name("company").send_keys(contact.company)
-            wd.find_element_by_name("address").click()
-            wd.find_element_by_name("address").clear()
-            wd.find_element_by_name("address").send_keys(contact.address)
-            wd.find_element_by_name("home").click()
-            wd.find_element_by_name("home").clear()
-            wd.find_element_by_name("home").send_keys(contact.home)
-            wd.find_element_by_name("mobile").clear()
-            wd.find_element_by_name("mobile").send_keys(contact.mobile)
-            wd.find_element_by_name("work").clear()
-            wd.find_element_by_name("work").send_keys(contact.work)
-            wd.find_element_by_name("fax").clear()
-            wd.find_element_by_name("fax").send_keys(contact.fax)
-            wd.find_element_by_name("email").clear()
-            wd.find_element_by_name("email").send_keys(contact.email)
-            wd.find_element_by_name("homepage").click()
-            wd.find_element_by_name("homepage").clear()
-            wd.find_element_by_name("homepage").send_keys(contact.homepage)
-            wd.find_element_by_name("bday").click()
-            Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-            wd.find_element_by_name("bmonth").click()
-            Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-            wd.find_element_by_name("bmonth").click()
-            wd.find_element_by_name("byear").click()
-            wd.find_element_by_name("byear").clear()
-            wd.find_element_by_name("byear").send_keys(contact.byear)
-            #wd.find_element_by_name("theform").click()
-            #wd.find_element_by_name("new_group").click()
-            #wd.find_element_by_name("theform").click()
-            wd.find_element_by_name("address2").click()
-            wd.find_element_by_name("address2").clear()
-            wd.find_element_by_name("address2").send_keys(contact.address2)
-            wd.find_element_by_name("phone2").click()
-            wd.find_element_by_name("phone2").clear()
-            wd.find_element_by_name("phone2").send_keys(contact.phone)
-            wd.find_element_by_name("notes").click()
-            wd.find_element_by_name("notes").clear()
-            wd.find_element_by_name("notes").send_keys(contact.notes)
             wd.find_element_by_name("update").click()
