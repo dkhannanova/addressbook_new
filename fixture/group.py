4383fae4efa9daf92a00626f94c8a74135c7b192
kdm <dkhannanovam@gmail.com>
2018-12-19 11:52:27 +0300

    заполнение формы выделено в отдельный метод

--- fixture/group.py
+++ fixture/group.py
@@ -7,12 +7,8 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
-    def create(self, group):
+    def fill_group(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group fields
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.group_name)
@@ -23,6 +19,14 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.group_footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group fields
+        self.fill_group(group)
         # submit group data
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -43,16 +47,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # modify group
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.group_name)
-        wd.find_element_by_xpath("//div[@id='content']/form").click()
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.group_header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
+        self.fill_group(group)
         # submit group data
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
