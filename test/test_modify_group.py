b84c0f5befb3c1ec745ccf86904c2a3b377058e0
kdm <dkhannanovam@gmail.com>
2018-12-19 19:27:43 +0300

    тест, изменяющий только один атриубут

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,5 +2,5 @@ from model.group import Group
 
 def test_modify_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_group(Group(group_name="modifname1", group_header="modifheader1", group_footer="modiffooter1"))
+    app.group.modify_group(Group(group_name="modifname3"))
     app.session.logout()
