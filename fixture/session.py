f3f55fc9e5f16d727fa2fafa72e9328741270adf
kdm <dkhannanovam@gmail.com>
2018-12-20 10:28:45 +0300

    добавлена проверка существования сессии

--- fixture/session.py
+++ fixture/session.py
@@ -1,10 +1,39 @@
+from fixture.application import Application
+
+
 class SessionHelper:
 
     def __init__(self, app):
         self.app = app
 
+    session = None
 
-    def login(self,username, password):
+    def login(self, username, password):
+        global session
+        wd = self.app.wd
+        if session is None:
+            wd = self.app.wd
+            self.app.open_home_page()
+            wd.find_element_by_name("user").clear()
+            wd.find_element_by_name("user").send_keys("%s" % username)
+            wd.find_element_by_name("pass").click()
+            wd.find_element_by_name("pass").clear()
+            wd.find_element_by_name("pass").send_keys("%s" % password)
+            wd.find_element_by_xpath("//input[@value='Login']").click()
+        else:
+            if not fixture.is_session_valid():
+                wd = self.app.wd
+                self.app.open_home_page()
+                wd.find_element_by_name("user").clear()
+                wd.find_element_by_name("user").send_keys("%s" % username)
+                wd.find_element_by_name("pass").click()
+                wd.find_element_by_name("pass").clear()
+                wd.find_element_by_name("pass").send_keys("%s" % password)
+                wd.find_element_by_xpath("//input[@value='Login']").click()
+
+
+
+    def login(self, username, password):
         wd = self.app.wd
         # login
         self.app.open_home_page()
