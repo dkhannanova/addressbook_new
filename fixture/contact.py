eba98e4d262668d791d492f055a7ec130f0da530
kdm <dkhannanovam@gmail.com>
2018-12-20 10:27:55 +0300

    исправление ошибки заполнения формы контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -16,11 +16,15 @@ class ContactHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
+    def change_field_file(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).send_keys(text)
+
+
     def change_field_select(self, field_name, text):
         wd = self.app.wd
         if text is not None:
-            wd.find_element_by_name(field_name).click()
-            wd.find_element_by_name(field_name).clear()
             Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
 
     def fill_contact(self, contact):
@@ -29,7 +33,7 @@ class ContactHelper:
         self.change_field("middlename", contact.middlename)
         self.change_field("lastname", contact.lastname)
         self.change_field("nickname", contact.nickname)
-        self.change_field("photo", contact.photo)
+        self.change_field_file("photo", contact.photo)
         self.change_field("title", contact.title)
         self.change_field("company", contact.company)
         self.change_field("address", contact.address)
@@ -39,19 +43,11 @@ class ContactHelper:
         self.change_field("fax", contact.fax)
         self.change_field("email", contact.email)
         self.change_field("homepage", contact.homepage)
-        self.change_field("bday", contact.bday)
-        self.change_field("work", contact.work)
-        self.change_field("work", contact.work)
-        self.change_field("work", contact.work)
-        self.change_field("work", contact.work)
-        self.change_field("work", contact.work)
-        self.change_field("work", contact.work)
-        self.change_field("work", contact.work)
         self.change_field_select("bday", contact.bday)
         self.change_field_select("bmonth", contact.bmonth)
         self.change_field("byear", contact.byear)
         self.change_field("address2", contact.address2)
-        self.change_field("phone", contact.phone)
+        self.change_field("phone2", contact.phone)
         self.change_field("notes", contact.notes)
 
     def create_new_contact(self, contact):
@@ -67,7 +63,6 @@ class ContactHelper:
             wd.find_element_by_name("selected[]").click()
             wd.find_element_by_xpath("//input[@value='Delete']").click()
             wd.switch_to_alert().accept()
-            self.return_to_contacts_page()
 
     def modify_contact(self, contact):
             wd = self.app.wd
