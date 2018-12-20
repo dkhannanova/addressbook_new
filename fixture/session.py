6f950befecf58c1a38409324f13a7427c40d4dc8
kdm <dkhannanovam@gmail.com>
2018-12-20 10:38:25 +0300

    to analyze error

--- fixture/session.py
+++ fixture/session.py
@@ -21,7 +21,7 @@ class SessionHelper:
             wd.find_element_by_name("pass").send_keys("%s" % password)
             wd.find_element_by_xpath("//input[@value='Login']").click()
         else:
-            if not fixture.is_session_valid():
+            if not fixture.application.is_session_valid():
                 wd = self.app.wd
                 self.app.open_home_page()
                 wd.find_element_by_name("user").clear()
@@ -33,16 +33,6 @@ class SessionHelper:
 
 
 
-    def login(self, username, password):
-        wd = self.app.wd
-        # login
-        self.app.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def logout(self):
         # logout
