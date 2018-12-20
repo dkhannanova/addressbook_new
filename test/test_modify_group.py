c8765739369cccac3012f4c8edb65683de0fd1f1
kdm <dkhannanovam@gmail.com>
2018-12-20 14:19:06 +0300

    Добавлена проверка существования группы/контакта для тестов модификации

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,4 +1,6 @@
 from model.group import Group
 
 def test_modify_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="name1", group_header="header1", group_footer="footer1"))
     app.group.modify_group(Group(group_name="modifname3"))
