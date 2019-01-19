6033030aeba02eb0e7babeb04997c7feed3473d3
kdm <dkhannanovam@gmail.com>
2019-01-19 19:03:42 +0300

    задание19

--- test/test_add_group_0812.py
+++ test/test_add_group_0812.py
@@ -4,9 +4,9 @@ import pytest
 from data.groups import testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 
-def test_add_group0812(app, group):
+def test_add_group0812(app, json_groups):
+    group = json_groups
     old_group = app.group.get_group_list()
     app.group.create(group)
     assert len(old_group) + 1 == app.group.count()
