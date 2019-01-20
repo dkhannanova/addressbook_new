2293d4f78ffba59cb4095be5a683d36da4d27a5c
kdm <dkhannanovam@gmail.com>
2019-01-20 11:10:49 +0300

    загрузка данных из файла

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,24 +5,9 @@ import pytest
 import random
 from model.contact import Contact
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + ""*10
-    return prefix+"".join(random.choice(symbols) for i in range(random.randrange(maxlen)))
 
-testdata = [Contact(firstname="", middlename="", lastname="", nickname="", photo=None, title="",
-                                            company="", address="", home="", mobile="", work="", fax="", email="",
-                                            homepage="", bday=None, bmonth=None, byear=None, address2="", phone="", notes="")] + [
-    Contact(firstname=random_string("first",10), middlename=random_string("middle",20), lastname=random_string("last", 13),
-          nickname=random_string("nick",12), photo="C:\\Users\hannanovadm\Pictures\9908383.pdf", title=random_string("title",12),
-          company=random_string("company",12), address=random_string("address",12), home=random_string("home",12), mobile=random_string("mobile",12),
-          work=random_string("work",12), fax=random_string("fax",12), email=random_string("email",12),
-          homepage=random_string("home",12), bday="1", bmonth="August", byear="1985", address2=random_string("address2",12), phone=random_string("phone",12), notes=random_string("notes",12))
-
-    for i in range (5)]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contact):
+    contact = json_contact
     old_contact = app.contact.get_contact_list()
     app.contact.create_new_contact(contact)
     assert len(old_contact)+1 == app.contact.count()
