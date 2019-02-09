33c02e39d3ab1b794e5c15c8cce96528cb3d7661
kdm <dkhannanovam@gmail.com>
2019-02-09 16:03:33 +0300

    тест соединения с бд

---
+++ check_connect.py
@@ -0,0 +1,12 @@
+import pymysql.cursors
+
+Connection = PyMySQL.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+try:
+    cursor=connection.cursor()
+#для просмотра структуры бд mysql: – вебстраницу localhost/phpmyadmin
+    cursor.execute("select * from group_list")
+# fetchall – возвращает все, что он извлек в виде набора строк.
+    for row in cursor.fetchall():
+	    print(row)
+finally:
+	connection.close()
